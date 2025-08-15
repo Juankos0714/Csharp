9c33593b87a5410b857e6872d578eeb489b086c0
Juan Camilo Rojas Ospina <144568676+Juankos0714@users.noreply.github.com>
2025-08-14 22:40:01 -0500

    Update calculadora.Designer.cs

--- WinFormsApp1/calculadora.Designer.cs
+++ WinFormsApp1/calculadora.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class calculadora
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +15,6 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             btnDiv = new Button();
@@ -47,221 +36,144 @@
             label1 = new Label();
             btnEquals = new Button();
             SuspendLayout();
-            // 
-            // btnDiv
-            // 
-            btnDiv.Font = new Font("Segoe UI", 48F);
-            btnDiv.Location = new Point(617, 341);
-            btnDiv.Name = "btnDiv";
-            btnDiv.Size = new Size(171, 97);
-            btnDiv.TabIndex = 0;
-            btnDiv.Text = "/";
-            btnDiv.UseVisualStyleBackColor = true;
-            btnDiv.Click += btnDiv_Click;
-            // 
-            // btnMult
-            // 
-            btnMult.Font = new Font("Segoe UI", 46F);
-            btnMult.Location = new Point(440, 341);
-            btnMult.Name = "btnMult";
-            btnMult.Size = new Size(171, 97);
-            btnMult.TabIndex = 1;
-            btnMult.Text = "X";
-            btnMult.UseVisualStyleBackColor = true;
-            btnMult.Click += btnMult_Click;
-            // 
-            // btnSum
-            // 
-            btnSum.Font = new Font("Segoe UI", 48F);
-            btnSum.Location = new Point(440, 238);
-            btnSum.Name = "btnSum";
-            btnSum.Size = new Size(171, 97);
-            btnSum.TabIndex = 2;
-            btnSum.Text = "+";
-            btnSum.UseVisualStyleBackColor = true;
-            btnSum.Click += btnSum_Click;
-            // 
-            // btnRest
-            // 
-            btnRest.Font = new Font("Segoe UI", 48F);
-            btnRest.Location = new Point(617, 238);
-            btnRest.Name = "btnRest";
-            btnRest.Size = new Size(171, 97);
-            btnRest.TabIndex = 3;
-            btnRest.Text = "-";
-            btnRest.UseVisualStyleBackColor = true;
-            btnRest.Click += btnRest_Click;
-            // 
-            // btn1
-            // 
-            btn1.Location = new Point(12, 12);
-            btn1.Name = "btn1";
-            btn1.Size = new Size(115, 99);
-            btn1.TabIndex = 4;
-            btn1.Text = "1";
-            btn1.UseVisualStyleBackColor = true;
-            btn1.Click += button1_Click;
-            // 
-            // btn2
-            // 
-            btn2.Location = new Point(133, 12);
-            btn2.Name = "btn2";
-            btn2.Size = new Size(115, 99);
-            btn2.TabIndex = 5;
-            btn2.Text = "2";
-            btn2.UseVisualStyleBackColor = true;
-            btn2.Click += btn2_Click;
-            // 
-            // btn3
-            // 
-            btn3.Location = new Point(254, 12);
-            btn3.Name = "btn3";
-            btn3.Size = new Size(115, 99);
-            btn3.TabIndex = 6;
-            btn3.Text = "3";
-            btn3.UseVisualStyleBackColor = true;
-            btn3.Click += btn3_Click;
-            // 
-            // btn6
-            // 
-            btn6.Location = new Point(254, 117);
-            btn6.Name = "btn6";
-            btn6.Size = new Size(115, 99);
-            btn6.TabIndex = 9;
-            btn6.Text = "6";
-            btn6.UseVisualStyleBackColor = true;
-            btn6.Click += btn6_Click;
-            // 
-            // btn5
-            // 
-            btn5.Location = new Point(133, 117);
-            btn5.Name = "btn5";
-            btn5.Size = new Size(115, 99);
-            btn5.TabIndex = 8;
-            btn5.Text = "5";
-            btn5.UseVisualStyleBackColor = true;
-            btn5.Click += btn5_Click;
-            // 
-            // btn4
-            // 
-            btn4.Location = new Point(12, 117);
-            btn4.Name = "btn4";
-            btn4.Size = new Size(115, 99);
-            btn4.TabIndex = 7;
-            btn4.Text = "4";
-            btn4.UseVisualStyleBackColor = true;
-            btn4.Click += btn4_Click;
-            // 
-            // btn9
-            // 
-            btn9.Location = new Point(254, 222);
-            btn9.Name = "btn9";
-            btn9.Size = new Size(115, 104);
-            btn9.TabIndex = 12;
-            btn9.Text = "9";
-            btn9.UseVisualStyleBackColor = true;
-            btn9.Click += btn9_Click;
-            // 
-            // btn8
-            // 
-            btn8.Location = new Point(133, 222);
-            btn8.Name = "btn8";
-            btn8.Size = new Size(115, 104);
-            btn8.TabIndex = 11;
-            btn8.Text = "8";
-            btn8.UseVisualStyleBackColor = true;
-            btn8.Click += btn8_Click;
-            // 
-            // btn7
-            // 
-            btn7.Location = new Point(12, 222);
-            btn7.Name = "btn7";
-            btn7.Size = new Size(115, 104);
-            btn7.TabIndex = 10;
-            btn7.Text = "7";
-            btn7.UseVisualStyleBackColor = true;
-            btn7.Click += btn7_Click;
-            // 
-            // btn0
-            // 
-            btn0.Location = new Point(133, 332);
-            btn0.Name = "btn0";
-            btn0.Size = new Size(115, 104);
-            btn0.TabIndex = 13;
-            btn0.Text = "0";
-            btn0.UseVisualStyleBackColor = true;
-            btn0.Click += btn0_Click;
-            // 
-            // btnDel
-            // 
-            btnDel.Location = new Point(254, 332);
-            btnDel.Name = "btnDel";
-            btnDel.Size = new Size(115, 104);
-            btnDel.TabIndex = 14;
-            btnDel.Text = "DEL";
-            btnDel.UseVisualStyleBackColor = true;
-            // 
-            // btnCe
-            // 
-            btnCe.Location = new Point(12, 332);
-            btnCe.Name = "btnCe";
-            btnCe.Size = new Size(115, 104);
-            btnCe.TabIndex = 15;
-            btnCe.Text = "CE";
-            btnCe.UseVisualStyleBackColor = true;
-            btnCe.Click += btnCe_Click;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(573, 117);
+
+            // ======== Pantalla ========
+            label1.BackColor = Color.Black;
+            label1.ForeColor = Color.White;
+            label1.Font = new Font("Segoe UI", 28F, FontStyle.Bold);
+            label1.TextAlign = ContentAlignment.MiddleRight;
+            label1.BorderStyle = BorderStyle.Fixed3D;
+            label1.Size = new Size(765, 60);
+            label1.Location = new Point(12, 20);
             label1.Name = "label1";
-            label1.Size = new Size(38, 15);
-            label1.TabIndex = 16;
-            label1.Text = "label1";
+            label1.Text = "";
             label1.Click += label1_Click;
-            // 
-            // btnEquals
-            // 
-            btnEquals.BackColor = Color.Transparent;
-            btnEquals.Font = new Font("Segoe UI", 48F);
-            btnEquals.ForeColor = Color.Black;
-            btnEquals.Location = new Point(806, 238);
+
+            // ======== Botones de operaciones ========
+            btnSum.Font = new Font("Segoe UI", 26F, FontStyle.Bold);
+            btnSum.Size = new Size(115, 90);
+            btnSum.BackColor = Color.SkyBlue;
+            btnSum.Location = new Point(440, 100);
+            btnSum.Name = "btnSum";
+            btnSum.Text = "+";
+            btnSum.UseVisualStyleBackColor = false;
+            btnSum.Click += btnSum_Click;
+
+            btnRest.Font = new Font("Segoe UI", 26F, FontStyle.Bold);
+            btnRest.Size = new Size(115, 90);
+            btnRest.BackColor = Color.SkyBlue;
+            btnRest.Location = new Point(565, 100);
+            btnRest.Name = "btnRest";
+            btnRest.Text = "-";
+            btnRest.UseVisualStyleBackColor = false;
+            btnRest.Click += btnRest_Click;
+
+            btnMult.Font = new Font("Segoe UI", 26F, FontStyle.Bold);
+            btnMult.Size = new Size(115, 90);
+            btnMult.BackColor = Color.SkyBlue;
+            btnMult.Location = new Point(440, 200);
+            btnMult.Name = "btnMult";
+            btnMult.Text = "X";
+            btnMult.UseVisualStyleBackColor = false;
+            btnMult.Click += btnMult_Click;
+
+            btnDiv.Font = new Font("Segoe UI", 26F, FontStyle.Bold);
+            btnDiv.Size = new Size(115, 90);
+            btnDiv.BackColor = Color.SkyBlue;
+            btnDiv.Location = new Point(565, 200);
+            btnDiv.Name = "btnDiv";
+            btnDiv.Text = "/";
+            btnDiv.UseVisualStyleBackColor = false;
+            btnDiv.Click += btnDiv_Click;
+
+            // ======== Botones numéricos ========
+            Button[] numButtons = { btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0 };
+            int[,] positions = {
+                {12, 100}, {133, 100}, {254, 100},
+                {12, 200}, {133, 200}, {254, 200},
+                {12, 300}, {133, 300}, {254, 300},
+                {133, 400}
+            };
+            for (int i = 0; i < numButtons.Length; i++)
+            {
+                numButtons[i].Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+                numButtons[i].Size = new Size(115, 90);
+                numButtons[i].BackColor = Color.LightGray;
+                numButtons[i].FlatStyle = FlatStyle.Flat;
+                numButtons[i].FlatAppearance.BorderSize = 0;
+                numButtons[i].Location = new Point(positions[i, 0], positions[i, 1]);
+                numButtons[i].UseVisualStyleBackColor = false;
+            }
+
+            btn1.Text = "1"; btn1.Click += button1_Click;
+            btn2.Text = "2"; btn2.Click += btn2_Click;
+            btn3.Text = "3"; btn3.Click += btn3_Click;
+            btn4.Text = "4"; btn4.Click += btn4_Click;
+            btn5.Text = "5"; btn5.Click += btn5_Click;
+            btn6.Text = "6"; btn6.Click += btn6_Click;
+            btn7.Text = "7"; btn7.Click += btn7_Click;
+            btn8.Text = "8"; btn8.Click += btn8_Click;
+            btn9.Text = "9"; btn9.Click += btn9_Click;
+            btn0.Text = "0"; btn0.Click += btn0_Click;
+
+            // ======== Botón DEL ========
+            btnDel.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            btnDel.Size = new Size(115, 90);
+            btnDel.BackColor = Color.Orange;
+            btnDel.ForeColor = Color.White;
+            btnDel.Location = new Point(254, 400);
+            btnDel.Name = "btnDel";
+            btnDel.Text = "DEL";
+            btnDel.UseVisualStyleBackColor = false;
+            btnDel.Click += btnDel_Click;
+
+            // ======== Botón CE ========
+            btnCe.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+            btnCe.Size = new Size(115, 90);
+            btnCe.BackColor = Color.Red;
+            btnCe.ForeColor = Color.White;
+            btnCe.Location = new Point(12, 400);
+            btnCe.Name = "btnCe";
+            btnCe.Text = "CE";
+            btnCe.UseVisualStyleBackColor = false;
+            btnCe.Click += btnCe_Click;
+
+            // ======== Botón igual ========
+            btnEquals.Font = new Font("Segoe UI", 26F, FontStyle.Bold);
+            btnEquals.Size = new Size(115, 190);
+            btnEquals.BackColor = Color.LightGreen;
+            btnEquals.Location = new Point(690, 100);
             btnEquals.Name = "btnEquals";
-            btnEquals.Size = new Size(171, 198);
-            btnEquals.TabIndex = 17;
             btnEquals.Text = "=";
             btnEquals.UseVisualStyleBackColor = false;
             btnEquals.Click += btnEquals_Click;
-            // 
-            // calculadora
-            // 
+
+            // ======== Form ========
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(989, 451);
-            Controls.Add(btnEquals);
+            ClientSize = new Size(820, 520);
             Controls.Add(label1);
-            Controls.Add(btnCe);
-            Controls.Add(btnDel);
-            Controls.Add(btn0);
-            Controls.Add(btn9);
-            Controls.Add(btn8);
-            Controls.Add(btn7);
-            Controls.Add(btn6);
-            Controls.Add(btn5);
-            Controls.Add(btn4);
-            Controls.Add(btn3);
-            Controls.Add(btn2);
-            Controls.Add(btn1);
-            Controls.Add(btnRest);
             Controls.Add(btnSum);
+            Controls.Add(btnRest);
             Controls.Add(btnMult);
             Controls.Add(btnDiv);
+            Controls.Add(btn1);
+            Controls.Add(btn2);
+            Controls.Add(btn3);
+            Controls.Add(btn4);
+            Controls.Add(btn5);
+            Controls.Add(btn6);
+            Controls.Add(btn7);
+            Controls.Add(btn8);
+            Controls.Add(btn9);
+            Controls.Add(btn0);
+            Controls.Add(btnDel);
+            Controls.Add(btnCe);
+            Controls.Add(btnEquals);
             Name = "calculadora";
-            Text = "calculadora";
+            Text = "Calculadora Mejorada";
             Load += calculadora_Load;
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
